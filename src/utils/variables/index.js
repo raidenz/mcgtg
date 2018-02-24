3806a10be4348a3bdc6c926e93e85248e9e1f911
Raidenz <th.deny@gmail.com>
2018-02-24 11:35:09 +0700

    add variables

---
+++ src/utils/variables/index.js
@@ -0,0 +1,30 @@
+const roman = {
+  "M": 1000,
+  "CM": 900,
+  "D": 500,
+  "CD": 400,
+  "C": 100,
+  "XC": 90,
+  "L": 50,
+  "XL": 40,
+  "X": 10,
+  "IX": 9,
+  "V": 5,
+  "IV": 4,
+  "I": 1
+};
+
+const galaxyType = {
+  "glob": "I",
+  "prok": "V",
+  "pish": "X",
+  "tegj": "L",
+}
+
+const creditValue = {
+  "silver": 17,  // 32 / 2
+  "gold": 14450, // 57796 / 4
+  "iron": 195.5  // 3890 / 20
+}
+
+const failText = "I have no idea what you are talking about"
